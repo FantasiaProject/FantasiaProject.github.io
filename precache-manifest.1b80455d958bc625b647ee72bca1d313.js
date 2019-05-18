e159f9212694bd1fb44bab47de6d55b8ee7d2e97
modelarious <michaelwilliamhackman@gmail.com>
2019-05-18 13:49:48 -0600

    first push of react project

---
+++ precache-manifest.1b80455d958bc625b647ee72bca1d313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a499841f4d0925a36b17",
+    "url": "/static/js/main.a499841f.chunk.js"
+  },
+  {
+    "revision": "56b47a562a31be415641",
+    "url": "/static/js/2.56b47a56.chunk.js"
+  },
+  {
+    "revision": "a499841f4d0925a36b17",
+    "url": "/static/css/main.a5a3afba.chunk.css"
+  },
+  {
+    "revision": "56b47a562a31be415641",
+    "url": "/static/css/2.3d855757.chunk.css"
+  },
+  {
+    "revision": "af29e52cc535fefdcb03e6cfa8f5eb23",
+    "url": "/index.html"
+  }
+];
